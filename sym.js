3edc95cbf3029c29c36f9db3751aea8f6ff37089
Qing Xu <joyeer.xuqing@gmail.com>
2013-02-23 15:11:57 +0800

    extends statement's class finding add apache 2.0 license header

--- sym.js
+++ sym.js
@@ -1,3 +1,19 @@
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one or more
+ *  contributor license agreements.  See the NOTICE file distributed with
+ *  this work for additional information regarding copyright ownership.
+ *  The ASF licenses this file to You under the Apache License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance with
+ *  the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
 var Visitor = require("./visitor").Visitor;
 var utils = require("./utils");
 var AST = require("./ast").AST;
@@ -29,8 +45,8 @@ SymVisitor.prototype.visitComplicationUnit = function(unit){
  */
 SymVisitor.prototype.visitClassDecl = function(decl){
 
-    decl.name = utils.isEmptyString(decl.parent.name) ?
-        decl.parent.name + "." +  decl.identifier.toString() : decl.identifier.toString();
+    decl.setName(utils.isEmptyString(decl.parent.name) ?
+        decl.parent.name + "." +  decl.identifier.toString() : decl.identifier.toString());
 
     var modifiers = [];
     // modifiers
@@ -63,7 +79,6 @@ SymVisitor.prototype.visitClassDecl = function(decl){
     }
     // make sure the modifiers doesn't contain annotations
     decl.modifiers = modifiers;
-    // extends
     this._symTab.registerClass(this._unit.name,decl.name,decl);
 
 };
